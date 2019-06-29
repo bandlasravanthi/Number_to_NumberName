7beab3e2cdd002dc07684021b7267aa0fcb02469
bandlasravanthi <sravs.bandla@gmail.com>
2019-06-29 23:48:15 +0530

    Number to Name changes

---
+++ NumberToName.aspx
@@ -0,0 +1,49 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NumberToName.aspx.cs" Inherits="NumberToNameConverter.NumberToName" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td>Enter Number: </td>
+
+                    <td>
+                        <input type="text" id="txtNumber" runat="server" /></td>
+                </tr>
+                <tr>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td>
+                        <asp:Button
+                            ID="Button1"
+                            runat="server"
+                            Text="Submit"
+                            OnClick="Button1_Click"
+                            Font-Bold="true"
+                            ForeColor="DodgerBlue"
+                           
+                            Width="150" /></td>
+                </tr>
+                <tr>
+                    <td style="height: 20%"></td>
+                </tr>
+                <tr>
+                    <td>Number Name: </td>
+
+                    <td>
+                        <asp:Label ID="txtNumberName" runat="server" Style="color: blue;"></asp:Label>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ NumberToName.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NumberToNameConverter
+{
+    public partial class NumberToName : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int number = Convert.ToInt32(txtNumber.Value);
+
+            string words = GetName(number);
+
+            txtNumberName.Text = words;
+        }
+
+        private string GetName(int number)
+        {
+            string words = "";
+            if (number == 0)
+                words = "zero";
+
+            else if (number < 0)
+                words = "minus " + GetName(Math.Abs(number));
+
+            else if ((number / 1000000) > 0)
+            {
+                words += GetName(number / 1000000) + " million ";
+                number %= 1000000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += GetName(number / 1000) + " thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += GetName(number / 100) + " hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                var unitsMap = new[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+                var tensMap = new[] { "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+
+                if (number < 20)
+                {
+                    words += unitsMap[number];
+                }
+                else
+                {
+                    words += tensMap[number / 10];
+                    if ((number % 10) > 0)
+                        words += "-" + unitsMap[number % 10];
+                }
+
+            }
+
+            return words;
+
+        }
+    }
+}
